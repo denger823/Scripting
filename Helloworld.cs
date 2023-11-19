e87f746f275689f668062693a780b91e9f311bef
denger823 <151452547+denger823@users.noreply.github.com>
2023-11-19 20:09:46 +0500

    Code with World

---
+++ Helloworld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class Helloworld: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Вывод в консоль приветствия миру
+        print("HelloWorld");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
